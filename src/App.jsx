f5f20dae9bce16503b4e0c4d37fb6fe764dd308e
Abdulkudus777 <Abdulkudusakanji777@gmail.com>
2024-11-09 08:25:26 +0000

    Add more UI to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-
-import ShoppingCart from './components/ShoppingCart'
+import ShoppingCart from "./components/ShoppingCart";
+// import Navbar from "./components/NavBar";
+// import Home from "./components/Home";
+// import Products from "./components/Products";
+// import About from "./components/About.jsx";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
-    <ShoppingCart/>
+      {/* <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/about" element={<About />} />
+        </Switch>
+      </Router> */}
+      <ShoppingCart />
     </>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
